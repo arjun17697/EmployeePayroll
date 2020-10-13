b28b4d68770941131fa1df5332991e1345333d94
arjun17697 <arjun17697@gmail.com>
2020-10-13 19:16:46 +0530

    Add:Functionality of Watch Service

--- src/com/bridgelabz/employee/NIOTest.java
+++ src/com/bridgelabz/employee/NIOTest.java
@@ -41,5 +41,12 @@ public class NIOTest {
 		Files.newDirectoryStream(playPath,path -> path.toFile().isFile() && path.toString().startsWith("temp")).
 		forEach(System.out::println);
 	}
+	
+	@Test
+	public void givenADirectoryWhenWatchedListsAlltheActivities() throws IOException{
+		Path dir =Paths.get(HOME+ "/"+PLAY_WITH_NIO);
+		Files.list(dir).filter(Files::isRegularFile).forEach(System.out::println);
+		new Java8WatchServiceExample(dir).processEvents();
+		}
 
 }
